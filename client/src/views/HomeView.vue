1600292a6c35e353f46cf1b66ed8e0d04c531cbd
Yash Ori <yash.ori498@gmail.com>
2022-04-20 00:28:32 +0100

    added login form

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="home">
-    <h1>Welcome to ✨OnlyBays✨</h1>
-    <h3>This project stemmed from our deep hatred of never knowing where to park</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, recusandae harum. Dolore maxime veniam pariatur aperiam exercitationem quia officia saepe nihil, consectetur possimus minima, repellendus iste harum ullam tempore nesciunt.</p>
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col">
+        <h1>Welcome to ✨OnlyBays✨</h1>
+        <h3>This project stemmed from our deep hatred of never knowing where to park</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima eius ad delectus tempore in non recusandae praesentium, odit doloribus eveniet omnis dolorem molestiae ipsam soluta voluptas mollitia amet beatae dolores.</p> 
+      </div>
+      
+      <div class="col login-form">
+        <h3>Login Here</h3>
+        <form>
+          <div class="mb-3"><input type="email" class="form-control" placeholder="Email"></div>
+          <div class="mb-3"><input type="password" class="form-control" placeholder="Password"></div>
+          <div class="mb-3"><button type="submit" class="btn btn-primary">Submit</button></div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import 'bootstrap/dist/css/bootstrap-grid.css'
 
 export default {
   name: 'HomeView',
-  // components: {}
 }
 </script>
 
