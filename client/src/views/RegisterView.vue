8d4ca180393cf58bc5275e08a191455f596d360a
Yash Ori <yash.ori498@gmail.com>
2022-05-09 15:15:20 +0100

    switch to balm ui

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,10 +1,10 @@
 <template>
-  <h1>Register View</h1>
+  
 </template>
 
 <script>
 export default {
-    name: 'RegisterView'
+
 }
 </script>
 
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.js',
+        'balm-ui-css': 'balm-ui/dist/balm-ui.css'
+      }
+    }
+  }
 })
