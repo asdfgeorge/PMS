714633db43ae1c7d07b82f4e7b7faedd9f459d02
Yash Ori <yash.ori498@gmail.com>
2022-05-09 15:15:20 +0100

    switch to balm ui

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
 
-createApp(App).use(router).mount('#app')
-import 'bootstrap/dist/js/bootstrap.js'
+import router from './router'
+import BalmUI from 'balm-ui'; // Official Google Material Components
+import BalmUIPlus from 'balm-ui-plus'; // BalmJS Team Material Components
+import 'balm-ui-css';
+
+const app = createApp(App)
+
+app.use(router)
+app.use(BalmUI)
+app.use(BalmUIPlus)
+
+app.mount('#app')
 
