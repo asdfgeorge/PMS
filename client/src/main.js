844e7b0c0e5b974fde28a4ce542d69d946136132
Yash Ori <yash.ori498@gmail.com>
2022-05-11 13:35:00 +0100

    starting point

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
 
 createApp(App).use(router).mount('#app')
-import 'bootstrap/dist/js/bootstrap.js'
 
