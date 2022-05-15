dbea24c4e51016459d67e1bdeba9da648cf69c05
JimJam117 <jamessparrow101@gmail.com>
2022-05-15 19:22:22 +0100

    login html

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,6 +5,25 @@
 
   <noAuth v-if="!store.auth" />
   <authed v-else />
+  
+  <section class="w-100 p-4 d-flex justify-content-center pb-4">
+      <form v-on:submit="SubmitLoginForm" style="width: 22rem;">
+        <!-- Email input -->
+        <div class="form-outline mb-4">
+          <input type="email" id="form2Example1" class="form-control">
+          <label class="form-label" for="form2Example1" style="margin-left: 0px;">Email address</label>
+        <div class="form-notch"><div class="form-notch-leading" style="width: 9px;"></div><div class="form-notch-middle" style="width: 97.6px;"></div><div class="form-notch-trailing"></div></div></div>
+
+        <!-- Password input -->
+        <div class="form-outline mb-4">
+          <input type="password" id="form2Example2" class="form-control">
+          <label class="form-label" for="form2Example2" style="margin-left: 0px;">Password</label>
+        <div class="form-notch"><div class="form-notch-leading" style="width: 9px;"></div><div class="form-notch-middle" style="width: 68px;"></div><div class="form-notch-trailing"></div></div></div>
+
+        <!-- Submit button -->
+        <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+      </form>
+    </section>
 
 </div>
 
@@ -23,6 +42,12 @@ export default {
     noAuth: noAuth,
     authed: authed
   }, 
+
+  SubmitLoginForm() {
+
+
+  }
+
 
   data() {
     return {
